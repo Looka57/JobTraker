f9775b7874b144766056478e8cb100d87d197f3f
Looka <napoa322@gmail.com>
2026-05-27 10:35:06 +0200

    Ajout de Chart.js et création du composant GraphiqueDashboard pour afficher des statistiques de candidatures

--- jobtracker-front/src/components/Statistiques.vue
+++ jobtracker-front/src/components/Statistiques.vue
@@ -1,4 +1,7 @@
 <script setup>
+
+import Button from 'primevue/button';
+
 </script>
 
 
@@ -9,16 +12,26 @@
         <div class="cardStat">
             <h3>Nombre total de candidatures envoyées</h3>
             <p>42</p>
+            <router-link to="/candidatures">
+                <Button label="En savoir plus" severity="info" variant="text" raised />
+                <!-- TODO: Ajouter l'ancre pour faire atterir le bon graphique selon le bouton cliqué         -->
+            </router-link>
         </div>
 
         <div class="cardStat">
             <h3>Taux de réponses</h3>
             <p>15</p>
+            <router-link to="/candidatures">
+                <Button label="En savoir plus" severity="info" variant="text" raised />
+            </router-link>
         </div>
 
         <div class="cardStat">
             <h3>Candidatures du mois</h3>
             <p>20</p>
+            <router-link to="/candidatures">
+                <Button label="En savoir plus" severity="info" variant="text" raised />
+            </router-link>
         </div>
     </div>
 </template>
